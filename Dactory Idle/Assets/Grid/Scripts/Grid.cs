da5891baa69129df44a4218112adecc3358fc637
Atahan <atahan012000@gmail.com>
2016-07-17 19:58:53 +0300

    1st day

--- Dactory Idle/Assets/Grid/Scripts/Grid.cs
+++ Dactory Idle/Assets/Grid/Scripts/Grid.cs
@@ -4,6 +4,8 @@ using System.Collections;
 [ExecuteInEditMode]
 public class Grid : MonoBehaviour {
 
+	public static Grid s;
+
 	public float gridScaleX = 1f;
 	public float gridScaleY = 1f;
 
@@ -12,46 +14,132 @@ public class Grid : MonoBehaviour {
 
 	public Color color = Color.white;
 
-	public Transform tilePrefab;
-
 	public TileSet tileSet;
 
-	public bool draggable;
+	GameObject[,] allTiles = new GameObject[10,10];
+	public GameObject emptyTile;
+
+	public enum TileTypes {dirt, grass, empty};
+
+	public TileTypes myType = TileTypes.empty;
+
+	public void Awake(){
+		s = this;
+	}
 
 	void Update(){
 		//transform.position = Vector3.up;
+		if (Application.isPlaying) {
 
 
+		} else {
+
+
+
+		}
 
 	}
 
-	void OnDrawGizmos(){
-		Vector3 pos = Camera.current.transform.position;
-		Gizmos.color = this.color;
+	public void UpdateTileSize(){
+		s = this;
+		DeleteAllTiles ();
+		allTiles = new GameObject[gridSizeX,gridSizeY];
+		ResetGrid ();
+	}
 
+	public void ResetGrid(){
+		DeleteAllTiles ();
 
-		/*for(float y = pos.y - 800.0f; y < pos.y + 800.0f; y+= this.gridScaleY){
-			Gizmos.DrawLine(new Vector3(-1000000.0f, Mathf.Floor(y/this.gridScaleY)*this.gridScaleY, 0.0f), 
-			                new Vector3(1000000.0f, Mathf.Floor(y/this.gridScaleY)*this.gridScaleY, 0.0f));
+		for (int x = 0; x < allTiles.GetLength (0); x++) {
+			for (int y = 0; y < allTiles.GetLength (1); y++) {
+
+				GameObject myTile = (GameObject)Instantiate (emptyTile, transform.position, transform.rotation);
+				allTiles [x, y] = myTile;
+
+				TileBaseScript myTileScript = myTile.GetComponent<TileBaseScript> ();
+
+				myTileScript.x = x;
+				myTileScript.y = y;
+				myTileScript.UpdateLocation ();
+
+				myTileScript.transform.parent = transform;
+			}
+		}
+	}
+
+	void DeleteAllTiles(){
+		foreach(GameObject gam in allTiles){
+			DestroyImmediate (gam);
 		}
 
-		for(float x = pos.x -1200.0f; x < pos.x + 1200.0f; x+= this.gridScaleX){
-			Gizmos.DrawLine(new Vector3(Mathf.Floor(x/this.gridScaleX)*this.gridScaleX,-1000000.0f, 0.0f), 
-			                new Vector3(Mathf.Floor(x/this.gridScaleX)*this.gridScaleX, 1000000.0f, 0.0f));
-		}*/
+		GameObject[] myChildren = new GameObject[transform.childCount];
+		int n = 0;
+		foreach (Transform child in transform) {
+			myChildren [n] = child.gameObject;
+			n++;
+		}
+
+		foreach(GameObject gam in myChildren){
+			DestroyImmediate (gam);
+		}
+	}
+
+	void OnDrawGizmos(){
+		//Vector3 pos = Camera.current.transform.position;
+		Gizmos.color = this.color;
 
 		for (int i = 0; i <= gridSizeX; i++) {
 			Gizmos.DrawLine (transform.position + new Vector3 (i * gridScaleX, 0, 0), 
 				transform.position + new Vector3 (i * gridScaleX, 0, 0) + new Vector3 (0, gridSizeY * gridScaleY, 0));
 		}
-
 		for (int i = 0; i <= gridSizeY; i++) {
 			Gizmos.DrawLine (transform.position + new Vector3 (0, i * gridScaleY, 0), 
 				transform.position + new Vector3 (0, i * gridScaleY, 0) + new Vector3 (gridSizeX * gridScaleX, 0, 0));
 		}
 
+		//origin
 		Gizmos.color = Color.red;
 				Gizmos.DrawSphere (transform.position, gridScaleX / 5);
+	}
+
+	public void ClickTile(GameObject tile){
+		s = this;
+		//print (tile);
+
+		TileBaseScript myTileScript = tile.GetComponent<TileBaseScript> ();
+
+		int x = myTileScript.x;
+		int y = myTileScript.y;
+
+		DestroyImmediate (allTiles [x, y].gameObject);
+
+		GameObject tileToSpawn;
+
+		switch (myType) {
+		case TileTypes.empty:
+			tileToSpawn = emptyTile;
+			break;
+		case TileTypes.dirt:
+			tileToSpawn = tileSet.prefabs [0];
+			break;
+		case TileTypes.grass:
+			tileToSpawn = tileSet.prefabs [1];
+			break;
+		default:
+			tileToSpawn = emptyTile;
+			break;
+		}
+
+		GameObject myTile = (GameObject)Instantiate (tileToSpawn, transform.position, transform.rotation);
+		allTiles [x, y] = myTile;
+
+		myTileScript = myTile.GetComponent<TileBaseScript> ();
+
+		myTileScript.x = x;
+		myTileScript.y = y;
+		myTileScript.UpdateLocation ();
+
+		myTileScript.transform.parent = transform;
 
 	}
 
---
+++ Dactory Idle/Assets/Tile/TileBaseScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileBaseScript : MonoBehaviour {
+
+	public int tileType = -1;
+	public int x = 0;
+	public int y = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void UpdateLocation(){
+		if (Grid.s != null) {
+			transform.position = Grid.s.transform.position +
+			new Vector3 (x * Grid.s.gridScaleX + Grid.s.gridScaleX / 2,
+				y * Grid.s.gridScaleY + Grid.s.gridScaleY / 2, 0);
+		} else {
+
+			Grid grid = GameObject.FindObjectOfType<Grid> ();
+
+			transform.position = grid.transform.position +
+				new Vector3 (x * grid.gridScaleX + grid.gridScaleX / 2,
+					y * grid.gridScaleY + grid.gridScaleY / 2, 0);
+
+		}
+	}
+}
