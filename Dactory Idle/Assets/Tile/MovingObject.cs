0af9ce4981d274734bf9d2434e15b43f703baa03
Atahan <atahan012000@gmail.com>
2016-07-25 21:00:56 +0300

    better belt movement and graphics

--- Dactory Idle/Assets/Tile/MovingObject.cs
+++ Dactory Idle/Assets/Tile/MovingObject.cs
@@ -5,17 +5,37 @@ public class MovingObject : MonoBehaviour {
 
 	public int type = 0;
 
-	public Vector3 placeToBe = Vector3.zero;
+	Vector3 randomOffset = Vector3.zero;
+
+	Vector3 _placeToBe = Vector3.zero;
+	public Vector3 placeToBe{
+		set{
+			//transform.position = _placeToBe;
+			_placeToBe = value;
+		}
+		get{
+			return _placeToBe;
+		}
+	}
+
 
 	// Use this for initialization
 	void Start () {
+
+		randomOffset = new Vector3(
+			Random.Range(-BeltSpeed.s.randomValue, BeltSpeed.s.randomValue),
+			Random.Range(-BeltSpeed.s.randomValue, BeltSpeed.s.randomValue),
+			0
+		);
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//transform.position = Vector3.Lerp (transform.position, placeToBe, 10f * Time.deltaTime);
-		transform.position = Vector3.MoveTowards (transform.position, placeToBe, 2f * Time.deltaTime);
+		if (BeltSpeed.s.mode)
+			transform.position = Vector3.Lerp (transform.position, placeToBe + randomOffset, BeltSpeed.s.speed * Time.deltaTime);
+		else
+			transform.position = Vector3.MoveTowards (transform.position, placeToBe  + randomOffset, BeltSpeed.s.speed * Time.deltaTime);
 	}
 
 	public void SelfDestruct(Vector3 offset){
